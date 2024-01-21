1ed1d8ffb9063c3401dccee40ab84d0c99f4b04b
Vishnu Murali <vishnumurali880@gmail.com>
2024-01-21 18:59:39 +0530

    updating Readme file

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# linkdein_scraping
+# LinkedIn Profile Scraper
+
+## Overview
+
+This project is a LinkedIn Profile Scraper that extracts information about people's profiles from LinkedIn. The scraper retrieves various details such as name, about, followers, location.
+
+**Note:** Scraping LinkedIn may violate LinkedIn's terms of service. Ensure that you are aware of and compliant with LinkedIn's policies before using this scraper.
+
+## Features
+
+- **Profile Information:** Extracts basic profile information, including name, headline, location, and profile URL.
+- **Name:** Retrieves the user's name.
+- **followers:** Gathers details about the user's followers.
+- **About:** Collects information about the user's background/intresr.
+- **location:** Retrieves the user's locations.
+
+## Prerequisites
+
+Before using the scraper, make sure you have the following installed:
+
+- Python 3.x
+- ChromeDriver (compatible with your Chrome browser version)
+- Required Python packages (specified in requirements.txt)
+
+## Setup
+
+1. Clone this repository:
+
+   ```cmd
+   git clone https://github.com/your-username/linkedin-scraper.git
+
+2. Install all the requirements
+
+    pip install -r requiremets.txt
+
+3. Run the python code
+
+    streamlit run app.py
--- app.py
+++ app.py
@@ -24,34 +24,41 @@ Followers =[]
 About = []
 def profile_info_linkdein(FirstName,LastName,EmailID,Password):
     #s=Service("C:/Users/DELL/Downloads/Dataset/chromedriver-win64/chromedriver.exe")
-    s=Service("chromedriver.exe")
+    s=Service("chromedriver.exe") ## Chrome driver extensions
     logging.info("Chrome driver path is executed")
     driver = webdriver.Chrome(service = s)
     logging.info("Hitting the linkdein url using selenium")
+    
+    # linkdein url
     link = f"https://www.linkedin.com/search/results/people/?heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAACtRLZMB7ymcym6TQImChrlOhl4BfCxYffw&keywords={FirstName}%20{LastName}&origin=SWITCH_SEARCH_VERTICAL&sid=L9b"
     driver.get(link)
     logging.info("The link is getting executed using selenium")
     time.sleep(2)
     
     logging.info("Entering linkdein login page")
+    
+    ## main linkdein path for login 
     driver.find_element_by_xpath("""/html/body/div[1]/main/div/p/a""").click()
     time.sleep(2)
     
     user_name = driver.find_element_by_xpath("""/html/body/div/main/div[2]/div[1]/form/div[1]/input""")
-    user_name.send_keys(EmailID)
+    ## linkdein url login details
+    user_name.send_keys(EmailID) ## provide email id
     user_name.send_keys(Keys.ENTER)
     logging.info("Email id is sucessfully inserted")
 
 
     password = driver.find_element_by_xpath("""/html/body/div/main/div[2]/div[1]/form/div[2]/input""")
-    password.send_keys(Password)
+    password.send_keys(Password) ## provide password
     password.send_keys(Keys.ENTER)
     logging.info('password is sucessfuly inserted')
 
 
     source = BeautifulSoup(driver.page_source,"html.parser")
+    ## scraping the profile using beautiful soup
     info = source.find("ul",{'class':'reusable-search__entity-result-list list-style-none'})
     profiles = source.find_all("li", {"class": "reusable-search__result-container"})
+    ## Loging to lindein page people section
     logging.info("Each box on linkdein profile is being shown")
     
     logging.info("Enetering into each profile")
@@ -59,6 +66,7 @@ def profile_info_linkdein(FirstName,LastName,EmailID,Password):
         try:
             logging.info("Entering to name profile")
             name = profile.find("span", {"aria-hidden": "true"}).get_text().strip()
+            ## retrving name from linkdein profile
             Name.append(name)
             logging.info("Name is executed sucessfully")
             print("Name :", name)
@@ -68,6 +76,7 @@ def profile_info_linkdein(FirstName,LastName,EmailID,Password):
             
         try:
             logging.info("Entering to location profile")
+            # retriving location from linkdein profile
             location = profile.find('div', {'class': 'entity-result__secondary-subtitle t-14 t-normal'}).get_text().strip()
             Location.append(location)
             logging.info("Location is executed sucessfully")
@@ -78,6 +87,7 @@ def profile_info_linkdein(FirstName,LastName,EmailID,Password):
 
         try:
             logging.info("Entering to followers of each profile")
+            #retriving followers from linkdein profile
             followers = profile.find('div', {'class': 'reusable-search-simple-insight__text-container'}).get_text().strip()
             Followers.append(followers)
             logging.info("followers is executed sucessfully")
@@ -91,6 +101,7 @@ def profile_info_linkdein(FirstName,LastName,EmailID,Password):
             if profile_name_span:
                 logging.info("Entering to about section")
                 about = profile_name_span.get_text().strip() if profile_name_span else "N/A"
+                # Retriving about from linkdein profile
                 About.append(about)
                 logging.info("About is executed sucessfully")
                 print("About:", about)
@@ -103,6 +114,7 @@ def profile_info_linkdein(FirstName,LastName,EmailID,Password):
         print("\n")
         
     d={'Name':Name,'Location':Location,'Followers':Followers,"About":About}
+    ## Creating data frame
     data = pd.DataFrame(d)
     data
 
@@ -111,14 +123,17 @@ def main():
     st.title("LinkedIn Profile Scraper")
 
     # Input fields
-    FirstName = st.text_input("Enter First Name:")
-    LastName = st.text_input("Enter Last Name:")
-    EmailID = st.text_input("Enter Email ID:")
-    Password = st.text_input("Enter Password:")
+    FirstName = st.text_input("Enter First Name:") ## Firstname
+    LastName = st.text_input("Enter Last Name:") ## lastname
+    EmailID = st.text_input("Enter Email ID:")  ## emailid/linkdein id
+    Password = st.text_input("Enter Password:") ## linkdein password
     
     if st.button("Scrape LinkedIn"):
         result_df = profile_info_linkdein(FirstName, LastName, EmailID,Password)
+        #final result
         st.dataframe(result_df)
 
+
+## executing the function
 if __name__ == "__main__":
     main()
